b298d9fe26cdfd162ee16c71188f384031f5d6a8
BRENO DIAS MACHADO <25132475@CFP127ALUNO.LOCAL>
2025-09-24 16:14:11 -0300

    começo das funçoes criadas

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -12,9 +12,30 @@
  * professor: Marcel
 */////////////////////////////////////////////////
 
-const dados = require('')
+const { status } = require('express/lib/response.js')
+const dados = require('./contatos.js')
+
+message = {status: false, statuscode: 500, development: 'Breno Dias Machado'}
 
 
+const getContadosAllDados() {
+
+
+message = {status: true, statuscode: 200, development: 'Breno Dias Machado', users = []}
+
+    dados.contatos['whats-users'].forEach(item) {
+        
+        let dadosUser = {}
+
+        dadosUser.id = item.id
+        dadosUser.account = item.account
+        dadosUser.nickname = item.nickname
+        dadosUser.
+
+    }
+
+
+}
 
 
 
